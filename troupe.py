c73f5f9f723a3b9404341cbd5330e645286810fa
flamschou <flams3630@gmail.com>
2023-11-06 15:05:28 +0100

    nvelle version

--- troupe.py
+++ troupe.py
@@ -1,30 +1,23 @@
 import pygame
+import hexagone_1 as hex
 
 
 class Troupe:
-    def __init__(self, troop_type, x, y):
+    def __init__(self, troop_type, hexagone):
         self.troop_type = troop_type
-        self.x = x
-        self.y = y
-        self.health = 100
-        self.attack_power = 10
+        self.hexagone = hexagone
+        self.health = 0
+        self.attack_power = 0
         self.status = "none"
+        self.speed = 0
+        self.color = (0, 0, 0)
 
-    def move(self, destination_x, destination_y):
-        delta_x = destination_x - self.x
-        delta_y = destination_y - self.y
-        distance = ((delta_x ** 2) + (delta_y ** 2)) ** 0.5
-        speed = 2
+    def move(self, destination_h):
+        speed = self.speed
 
-        if distance > 0:
-            move_x = (delta_x / distance) * speed
-            move_y = (delta_y / distance) * speed
-            self.x += move_x
-            self.y += move_y
-
-            if abs(self.x - destination_x) < move_x and abs(self.y - destination_y) < move_y:
-                self.x = destination_x
-                self.y = destination_y
+        if  self.hexagone.voisin(destination_h) & speed >0:
+            self.hexagone = destination_h
+            speed -= 1
 
     def attack(self, target):
         if isinstance(target, Troupe):
@@ -35,14 +28,35 @@ class Troupe:
                 target.status = "dead"
 
     def draw(self, screen):
-        color_mapping = {
-            "assassin": (255, 0, 0),
-            "magician": (0, 0, 255),
-            "turret": (0, 255, 0)
-        }
-
         troop_center_x = self.x
         troop_center_y = self.y
         troop_radius = 20
 
-        pygame.draw.circle(screen, color_mapping[self.troop_type], (troop_center_x, troop_center_y), troop_radius)
+        pygame.draw.circle(screen, self.color, (troop_center_x, troop_center_y), troop_radius)
+
+
+class Assassin(Troupe):
+    def __init__(self, x, y):
+        super().__init__("assassin", x, y)
+        self.color = (255, 0, 0)
+        self.health = 100
+        self.attack_power = 20
+        self.speed = 5
+
+
+class Magician(Troupe):
+    def __init__(self, x, y):
+        super().__init__("magician", x, y)
+        self.health = 200
+        self.attack_power = 50
+        self.speed = 3
+        self.color = (0, 0, 255)
+
+
+class Turret(Troupe):
+    def __init__(self, x, y):
+        super().__init__("turret", x, y)
+        self.health = 500
+        self.attack_power = 100
+        self.speed = 1
+        self.color = (0, 255, 0)
