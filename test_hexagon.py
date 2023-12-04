c08862e5cb92fdb054ce03e5f99be4eca08ef1f5
flamschou <flams3630@gmail.com>
2023-12-04 15:25:29 +0100

    pytest

--- test_hexagon.py
+++ test_hexagon.py
@@ -5,38 +5,42 @@ from os import path
 
 def test___init__():
     hexagon = Rock(10, 15)
+    image = pygame.image.load(path.join("Images", "hexagon rock.png"))
+    image = pygame.transform.scale(image, (60, 60))
+
     assert hexagon.hex_type == "rock"
     assert hexagon.x == 10
     assert hexagon.y == 15
     assert not hexagon.occupied
     assert hexagon.index is None
-    assert hexagon.image == pygame.transform.scale(
-        pygame.image.load(path.join("Images", "hexagon rock.png"), (60, 60))
-    )
+    assert hexagon.image == image
 
 
 def test_toBasic():
     hexagon = Hexagone("None", 10, 15)
     hexagon.toBasic()
+    image = pygame.image.load(path.join("Images", "hexagon basic.png"))
+    image = pygame.transform.scale(image, (60, 60))
+
     assert hexagon.hex_type == "basic"
-    assert hexagon.image == pygame.transform.scale(
-        pygame.image.load(path.join("Images", "hexagon basic.png"), (60, 60))
-    )
+    assert hexagon.image == image
 
 
 def test_toSand():
     hexagon = Hexagone("None", 10, 15)
     hexagon.toSand()
+    image = pygame.image.load(path.join("Images", "hexagon sand.png"))
+    image = pygame.transform.scale(image, (60, 60))
+
     assert hexagon.hex_type == "sand"
-    assert hexagon.image == pygame.transform.scale(
-        pygame.image.load(path.join("Images", "hexagon sand.png"), (60, 60))
-    )
+    assert hexagon.image == image
 
 
 def test_toDefended():
     hexagon = Hexagone("None", 10, 15)
     hexagon.toDefended()
+    image = pygame.image.load(path.join("Images", "hexagon defended.png"))
+    image = pygame.transform.scale(image, (60, 60))
+
     assert hexagon.hex_type == "defended"
-    assert hexagon.image == pygame.transform.scale(
-        pygame.image.load(path.join("Images", "hexagon defended.png"), (60, 60))
-    )
+    assert hexagon.image == image
