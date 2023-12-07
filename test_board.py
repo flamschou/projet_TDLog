d2f43ef0e2f4a8c00ccca18422dd0b555898d59e
flamschou <flams3630@gmail.com>
2023-12-07 15:36:53 +0100

    test

--- test_board.py
+++ test_board.py
@@ -8,13 +8,14 @@ def test_generate_board():
     board = Board()
     board.generate_board(3, 3)
     assert len(board.list) == 9
-    for i in range(9):
-        assert board.list[i].index == i
-        assert board.list[i].hex_type in ["basic", "sand", "forest", "rock"]
-        assert (
-            board.list[i].x == i * 60 * S + (30 * S if i % 2 == 0 else 60 * S) + 80 * S
-        )
-        assert board.list[i].y == 100 * S + (i // 3) * 52 * S
+    for col in range(3):
+        for row in range(3):
+            assert board.list[row * 3 + col].index == row * 3 + col
+            assert board.list[row * 3 + col].hex_type in ["basic", "sand", "forest", "rock"]
+            assert board.list[row * 3 + col].x == col * 60 * S + (
+                30 * S if row % 2 == 0 else 60 * S
+            ) + 80 * S
+            assert board.list[row * 3 + col].y == row * 52 * S + 100 * S
 
 
 def test_list_neighbors():
@@ -60,7 +61,7 @@ def test_isdistance():
     assert not board.isdistance(board.list[0], board.list[8], 2)
 
 
-'''
+"""
 def test_larger_list_neighbors():
     board = Board()
     board.generate_board(3, 3)
@@ -73,4 +74,4 @@ def test_larger_list_neighbors():
     assert len(board.larger_list_neighbors(board.list[6])) == 4
     assert len(board.larger_list_neighbors(board.list[7])) == 6
     assert len(board.larger_list_neighbors(board.list[8])) == 4
-'''
+"""
